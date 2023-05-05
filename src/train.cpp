30e79e1a6de4a0d84eb8e394c053ccec478a5730
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-06 00:11:41 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,51 +1,55 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() {
-  first = nullptr;
-  countOp = 0;
-}
+Train::Train() : countOp(0), first(nullptr) {}
 
 void Train::addCage(bool light) {
-  Cage* newCage = new Cage();
-  newCage->light = light;
-  if (!first) {
-    newCage->next = newCage;
-    newCage->prev = newCage;
-    first = newCage;
-  } else {
-    newCage->next = first;
-    newCage->prev = first->prev;
-    first->prev->next = newCage;
-    first->prev = newCage;
-  }
+    if (first == nullptr) {
+        first = new Cage;
+        first->next = first;
+        first->prev = first;
+        first->light = light;
+    } else {
+        Cage* newCage = new Cage;
+        newCage->prev = first->prev;
+        newCage->next = first;
+        first->prev->next = newCage;
+        first->prev = newCage;
+        newCage->light = light;
+    }
 }
 
 int Train::getLength() {
-  if (!first) {
-    return 0;
-  }
-  int length = 0;
-  Cage* current = first;
-  do {
-    length++;
-    current = current->next;
-    countOp++;
-  } while (current != first);
-  return length;
+    int length = 0;
+    if (first != nullptr) {
+        Cage* cur = first;
+        do {
+            length++;
+            cur = cur->next;
+        } while (cur != first);
+    }
+    return length;
 }
 
 int Train::getOpCount() {
-  return countOp;
-}
-
-Train::~Train() {
-  if (first) {
-    Cage* current = first;
-    do {
-      Cage* temp = current;
-      current = current->next;
-      delete temp;
-    } while (current != first);
-  }
+    int opCount = 0;
+    if (first != nullptr) {
+        Cage* cur = first;
+        do {
+            if (cur->light == true) {
+                cur->light = false;
+                opCount += 2;
+                Cage* temp = cur;
+                while (temp->next != cur) {
+                    temp = temp->next;
+                    temp->light = !temp->light;
+                    opCount++;
+                }
+            }
+            cur = cur->next;
+            opCount++;
+        } while (cur != first);
+    }
+    countOp = opCount;
+    return opCount;
 }
