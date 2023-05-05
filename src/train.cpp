a6a54bd351906fc730eaa1f7bffa88b1e821449e
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-06 01:13:23 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() : countOp(0), first(nullptr) {}
+Train::Train() : countOp(0), opCount(0), first(nullptr) {}
 
 void Train::addCage(bool light) {
   Cage* item = new Cage;
