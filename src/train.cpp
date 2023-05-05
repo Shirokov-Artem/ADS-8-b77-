26bc2eb8294b997b1c97220e9cab6dc194dd1d21
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-05 17:20:03 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -2,58 +2,49 @@
 #include "train.h"
 
 Train::Train() {
-    first = nullptr;
-    countOp = 0;
+  first = nullptr;
+  countOp = 0;
 }
 
 void Train::addCage(bool light) {
-    Cage* newCage = new Cage();
-    newCage->light = light;
-    if (first == nullptr) {
-        newCage->next = newCage;
-        newCage->prev = newCage;
-        first = newCage;
-    } else {
-        newCage->next = first;
-        newCage->prev = first->prev;
-        first->prev->next = newCage;
-        first->prev = newCage;
-    }
+  Cage* newCage = new Cage();
+  newCage->light = light;
+  if (!first) {
+    newCage->next = newCage;
+    newCage->prev = newCage;
+    first = newCage;
+  } else {
+    newCage->next = first;
+    newCage->prev = first->prev;
+    first->prev->next = newCage;
+    first->prev = newCage;
+  }
 }
 
 int Train::getLength() {
-    int length = 0;
-    Cage* current = first;
-    Cage* last = current->prev;   
-    while (current != nullptr) {
-        if (current->light) {
-            if (length == 1) {
-                length--;
-                countOp++;
-            } else if (length > 1) {
-                for (int i = 0; i < length - 1; i++) {
-                    last = last->prev;
-                    countOp++;
-                }
-                last->next = current->next;
-                current->next->prev = last;
-                Cage* temp = current->next;
-                delete current;
-                current = temp;
-                length = 0;
-            } else {
-                current = current->next;
-            }
-        } else {
-            length++;
-            last = current;
-            current = current->next;
-        }
-        countOp++;
-    }
-    return length;
+  if (!first) {
+    return 0;
+  }
+  int length = 0;
+  Cage* current = first;
+  do {
+    length++;
+    current = current->next;
+  } while (current != first);
+  return length;
 }
 
 int Train::getOpCount() {
-    return countOp;
+  return countOp;
+}
+
+Train::~Train() {
+  if (first) {
+    Cage* current = first;
+    do {
+      Cage* temp = current;
+      current = current->next;
+      delete temp;
+    } while (current != first);
+  }
 }
