991e20e5e7efdc8e502daebddb4f5ab10bd2165f
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-06 01:19:31 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -10,12 +10,12 @@ class Train {
     Cage *prev;
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  int opCount;
   Cage *first; // точка входа в поезд (первый вагон)
  public:
   Train();
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
+  ~Train();
 };
 #endif  // INCLUDE_TRAIN_H_
