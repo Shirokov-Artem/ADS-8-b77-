f7867d76572dc8c81937917b23c76c558224eb19
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-07 02:08:13 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,60 +1,58 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() : first(nullptr), countOp(-1) {}
+Train::Train() : countOp(-1), first(nullptr) {}
 
 void Train::addCage(bool light) {
-  Cage* newCage = new Cage{light, nullptr, nullptr};
   if (first == nullptr) {
-    first = newCage;
-    first->next = first;
-    first->prev = first;
+    first = new Cage{light, nullptr, nullptr};
+    first->next = first->prev = first;
   } else {
-    newCage->prev = first->prev;
+    Cage* newCage = new Cage{light, first, first->prev};
     first->prev->next = newCage;
     first->prev = newCage;
-    newCage->next = first;
   }
 }
 
 int Train::getLength() {
-  if (length > 0) {
-    return length;
-  } else {
-    int len = 0;
-    Cage* currentCage = first;
-    bool flag = false;
-    while (!flag || currentCage != first) {
-      flag = true;
-      ++countOp;
-      if (!currentCage->light) {
-        ++len;
-        currentCage = currentCage->next;
-      } else {
-        Cage* moveCage = currentCage->prev;
-        for (int i = 0; i < len - 1; ++i) {
-          ++countOp;
-          moveCage = moveCage->prev;
-        }
-        ++countOp;
-        if (moveCage == first) {
-          break;
+  if (countOp == -1) {
+    countOp = 0;
+    if (first != nullptr) {
+      Cage* currentCage = first;
+      int length = 0;
+      bool isLightOn = false;
+      do {
+        if (!isLightOn) {
+          length++;
+          currentCage = currentCage->next;
         } else {
-          currentCage = moveCage->next;
-          len = 1;
+          int steps = length - 1;
+          Cage* prevCage = currentCage;
+          while (steps--) {
+            countOp++;
+            prevCage = prevCage->prev;
+          }
+          if (prevCage == first) {
+            break;
+          } else {
+            currentCage = prevCage->next;
+            length = 1;
+          }
         }
-      }
+        isLightOn = !isLightOn;
+        } while (currentCage != first);
+      countOp++;
+      return length;
     }
-    length = len;
-    return length;
+    return 0;
+  } else {
+    return countOp;
   }
 }
 
 int Train::getOpCount() {
-  if (countOp > -1) {
-    return countOp;
-  } else {
+  if (countOp == -1) {
     getLength();
-    return countOp;
   }
+  return countOp;
 }
