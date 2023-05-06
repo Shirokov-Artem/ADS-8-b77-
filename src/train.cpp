cd3ce74f2a9e6502e51c1b8af13a459de8e85094
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-07 01:42:09 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -31,38 +31,39 @@ int Train::getLength() {
             currLen = 1;
             Cage *nextCage = currCage->next;
             while (nextCage != currCage && nextCage->light == true) {
-                currLen += 1;
+                currLen++;
                 nextCage = nextCage->next;
             }
             if (currLen > maxLen)
                 maxLen = currLen;
         }
         currCage = currCage->next;
+        countOp++;
     } while (currCage != first);
     return maxLen;
 }
 
 int Train::getOpCount() {
     if (countOp == 0) {
-        countOp = 0;
-        if (first != nullptr) {
-            Cage *currCage = first;
-            do {
-                if (currCage->light == true && currCage->next->light == true) {
-                    currCage->light = false;
-                    currCage->next->light = false;
-                    countOp += 2;
-                    if (currCage->prev == currCage) {
-                        currCage = currCage->next;
-                    } else {
-                        currCage = currCage->prev;
-                    }
-                } else {
-                    currCage = currCage->next;
-                    countOp += 1;
-                }
-            } while (currCage != first);
+        countOp++;
+        Cage *currCage = first;
+        while (currCage->light != true) {
+            currCage = currCage->next;
+            countOp++;
         }
+        currCage->light = false;
+        
+        bool found = false;
+        while (!found) {
+            if (currCage->light == true) {
+                currCage->light = false;
+                found = true;
+            } else {
+                currCage = currCage->next;
+                countOp++;
+            }
+        }
+        getLength();
     }
     return countOp;
 }
