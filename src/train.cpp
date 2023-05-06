01f235b9136eebe41283dd18ef0748256a18cb87
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-07 02:14:52 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,58 +1,52 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() : countOp(-1), first(nullptr) {}
+Train::Train() : countOp(0), first(nullptr) {}
 
 void Train::addCage(bool light) {
-  if (first == nullptr) {
-    first = new Cage{light, nullptr, nullptr};
-    first->next = first->prev = first;
-  } else {
-    Cage* newCage = new Cage{light, first, first->prev};
-    first->prev->next = newCage;
-    first->prev = newCage;
-  }
+    if (first == nullptr) {
+        first = new Cage{light, nullptr, nullptr};
+        first->next = first->prev = first;
+    } else {
+        Cage* prevLastCage = first->prev;
+        prevLastCage->next = new Cage{ light, first, prevLastCage };
+        first->prev = prevLastCage->next;
+    }
 }
 
 int Train::getLength() {
-  if (countOp == -1) {
-    countOp = 0;
-    if (first != nullptr) {
-      Cage* currentCage = first;
-      int length = 0;
-      bool isLightOn = false;
-      do {
-        if (!isLightOn) {
-          length++;
-          currentCage = currentCage->next;
-        } else {
-          int steps = length - 1;
-          Cage* prevCage = currentCage->prev;
-          while (steps--) {
-            countOp++;
-            prevCage = prevCage->prev;
-          }
-          if (prevCage == first) {
-            break;
-          } else {
-            currentCage = prevCage->next;
-            length = 1;
-          }
+    if (countOp == 0) {
+        if (first == nullptr) {
+            return 0;
         }
-        isLightOn = !isLightOn;
+        int length = 0;
+        Cage* currentCage = first;
+        do {
+            if (!currentCage->light) {
+                length++;
+            } else {
+                Cage* prevCage = currentCage->prev;
+                int i = length - 1;
+                while (i--) {
+                    prevCage = prevCage->prev;
+                    countOp++;
+                }
+                if (prevCage == first->prev) {
+                    break;
+                }
+                length = 1;
+                currentCage = prevCage->next;
+            }
+            currentCage = currentCage->next;
         } while (currentCage != first);
-      countOp++;
-      return length;
+        countOp += length;
+        return length;
+    } else {
+        return countOp;
     }
-    return 0;
-  } else {
-    return countOp;
-  }
 }
 
 int Train::getOpCount() {
-  if (countOp == -1) {
     getLength();
-  }
-  return countOp;
+    return countOp;
 }
