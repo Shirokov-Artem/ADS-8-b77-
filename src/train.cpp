9ee63be8b552bb5a785255827bace7a230c40688
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-07 17:17:40 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -4,36 +4,35 @@
 Train::Train() : countOp(0), first(nullptr) {}
 
 void Train::addCage(bool light) {
-    Cage* cage = new Cage{light};
-    if (first == nullptr) {
-        first = cage;
-        first->next = cage;
-        first->prev = cage;
-    } else {
-        cage->next = first;
-        cage->prev = first->prev;
-        first->prev->next = cage;
-        first->prev = cage;
-    }
+  Cage* cage = new Cage{ light };
+  if (first == nullptr) {
+    first = cage;
+    first->next = cage;
+    first->prev = cage;
+  } else {
+    cage->next = first;
+    cage->prev = first->prev;
+    first->prev->next = cage;
+    first->prev = cage;
+  }
 }
 
 int Train::getLength() {
-    int length = 0;
-    bool curState = first->light;
-    Cage* curCage = first;
-    do {
-        if (curCage->light != curState) {
-            length++;
-            curState = !curState;
-        }
-        curCage = curCage->next;
-    } while (curCage != first);
-    if (curState != first->light) {
-        length++;
+  int length = 0;
+  bool curState = first->light;
+  Cage* curCage = first;
+  do {
+    length++;
+    curCage = curCage->next;
+    if (curCage->light != curState) {
+      curState = !curState;
+      length++;
     }
-    return length;
+  } while (curCage != first);
+
+  return length;
 }
 
 int Train::getOpCount() {
-    return countOp;
+  return countOp;
 }
